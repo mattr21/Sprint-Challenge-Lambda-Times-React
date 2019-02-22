cf3afd667a8beacf0f702741fd7a5e046f202dfd
mattr21 <matt.rothstein21@gmail.com>
2019-02-22 08:11:13 -0800

    completed componentDidMount in Content tab to pull in data to state

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Tab from './Tab';
+
 const Tabs = props => {
+  console.log(props, "Tabs")
   return (
+    
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
