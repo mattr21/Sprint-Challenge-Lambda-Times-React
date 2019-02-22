1fd4b99f9c3602cd0ec7fef06cc58a4939046979
mattr21 <matt.rothstein21@gmail.com>
2019-02-22 09:14:07 -0800

    added proptypes type checking to the cards and card components

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 const Card = props => {
   // console.log(props, "Card")
@@ -15,6 +16,13 @@ const Card = props => {
   );
 };
 
-// Make sure to include PropTypes.
+Card.propTypes = {
+  card: PropTypes.shape({
+    author: PropTypes.string,
+    headline: PropTypes.string,
+    img: PropTypes.string,
+    tab: PropTypes.string
+  })
+}
 
 export default Card;
--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -2,13 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Tab = props => {
-  console.log(props, "Tab")
-
-  // ********************** I THINK I'M DONE WITH THIS... **********************
-  /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
-      if they match, the className should be: 'tab active-tab', 
-      if it is not it should just be 'tab'*/
-
+  // console.log(props, "Tab")
       
   return (
     <div
@@ -22,7 +16,6 @@ const Tab = props => {
   );
 };
 
-// Make sure you include PropTypes on your props.
 Tab.propTypes = {
   selectedTab: PropTypes.string,
   tab: PropTypes.string,
